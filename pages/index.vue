98f6f1c6a1b4eb03bb441764b5ec4131419da95f
amapic <amaury.pichat@gmail.com>
2024-01-12 13:35:48 +0100

    first_commit

---
+++ pages/index.vue
@@ -0,0 +1,31 @@
+<template>
+  <div class="home">
+    <h1>{{ "message" }}</h1>
+    <p>Welcome to my responsive Nuxt.js website!</p>
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from '@nuxt/vue-app';
+
+export default defineComponent({
+  data() {
+    return {
+      message: 'Hello, Nuxt.js with TypeScript!',
+    };
+  },
+});
+</script>
+
+<style scoped>
+.home {
+  text-align: center;
+  padding: 20px;
+}
+
+@media (max-width: 600px) {
+  .home {
+    font-size: 14px;
+  }
+}
+</style>
